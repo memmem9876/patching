c9fa8a1e4f9759e6333d07daa7eb74da57ab79c2
Your Name <you@example.com>
2024-10-29 11:44:30 +0900

    api

--- install.py
+++ install.py
@@ -34,7 +34,7 @@ SUPPORTED_ENVIRONMENT = bool(SUPPORTED_IDA and SUPPORTED_PYTHON)
 #------------------------------------------------------------------------------
 
 PLUGIN_NAME = 'Patching'
-PLUGIN_URL = 'https://api.github.com/repos/memmem9876/patching/releases/latestgg'
+PLUGIN_URL = 'https://api.github.com/repos/memmem9876/patching/releases/latest'
 
 def install_plugin():
     """
